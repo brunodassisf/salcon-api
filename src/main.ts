4f43f8c27e8d2f110fc2cc2c813950af6705830b
brunodassisf <brunodassis30@gmail.com>
2023-03-10 18:50:36 -0300

    checando se ambiente e de produção ou de desenvolvimento

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const isProduction = process.env.NODE_ENV === 'production';
+
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3001);
+  await app.listen(isProduction ? 3000 : 3001);
 }
 bootstrap();
