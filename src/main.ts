e31a36e2f63038726851916597f439e3b6b08db8
brunodassisf <brunodassis30@gmail.com>
2023-03-11 12:48:10 -0300

    colocando env necessaria para deploy na heroku

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const isProduction = process.env.NODE_ENV === 'production';
-
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(isProduction ? 3000 : 3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
